b39358daafdb4214f522a5ff15d569f5f8d36cad
danielfragareis <danielfreis91@gmail.com>
2023-11-03 21:50:38 -0300

    Modificando os arquivos do Java para php

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -1,30 +1,65 @@
 <?php
-
 namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
- * Does the scrapping of a webpage.
+ * Scraps paper information from HTML and returns an array with the data.
  */
-class Scrapper {
+class Scrapper
+{
 
   /**
-   * Loads paper information from the HTML and returns the array with the data.
+   * Loads paper information from the HTML and returns an array with the data.
+   *
+   * @return array of Papers with papers information
+   * @param $dom
    */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
-  }
+  public function scrap(\DOMDocument $dom): array
+  {
+    $aTags = $dom->getElementsByTagName('a');
+    //echo 'Debug: Found ' . count($aTags) . ' <a> tags in the document.' . PHP_EOL;
+    $papersList = [];
 
+
+    foreach ($aTags as $aTag) {
+      if ($aTag->getAttribute('class') === 'paper-card p-lg bd-gradient-left') {
+        $divVolumeInfo = (int) $aTag->getElementsByTagName('div')->item(0)->nodeValue;
+        $university = $aTag->getElementsByTagName('span')->item(0)->getAttribute('title');
+        $paperTitle = $aTag->getElementsByTagName('h4')->item(0)->nodeValue;
+        $paperType = $aTag->getElementsByTagName('div')->item(1)->nodeValue;
+        $authors = $aTag->getElementsByTagName('div')->item(2)->nodeValue;
+
+        echo 'Debug: divVolumeInfo: ' . $aTag->getElementsByTagName('div')->item(0)->nodeValue . PHP_EOL;
+        echo 'Debug: university: ' . $aTag->getElementsByTagName('span')->item(0)->getAttribute('title') . PHP_EOL;
+        echo 'Debug: paperTitle: ' . $aTag->getElementsByTagName('h4')->item(0)->nodeValue . PHP_EOL;
+        echo 'Debug: paperType: ' . $aTag->getElementsByTagName('div')->item(1)->nodeValue . PHP_EOL;
+        echo 'Debug: authors: ' . $aTag->getElementsByTagName('div')->item(2)->nodeValue . PHP_EOL;
+        $authorsList = [];
+
+        foreach (explode(';', $authors) as $authorInfo) {
+          echo 'Debug: Found a ";" separator in authors: ' . $authorInfo . PHP_EOL;
+          $authorInfo = trim($authorInfo);
+          $authorInfoParts = explode(',', $authorInfo, 2);
+          if (count($authorInfoParts) >= 2) {
+            echo '-------------------------------------------------';
+            list($authorName, $university) = $authorInfoParts;
+            $authorsList[] = new Person(trim($authorName), trim($university));
+          } else {
+            // Lida com casos em que as partes não estão disponíveis
+            echo 'Debug: Missing author or university information' . PHP_EOL;
+          }
+          //list($authorName, $university) = explode(',', $authorInfo, 2);
+          //$authorsList[] = new Person(trim($authorName), trim($university));
+        }
+
+        $papersList[] = new Paper($divVolumeInfo, $paperTitle, $paperType, $authorsList);
+      }
+    }
+
+    return $papersList;
+  }
 }
+?>
