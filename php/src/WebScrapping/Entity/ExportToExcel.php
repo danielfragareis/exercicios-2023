e70cab2e0d5bc81cbdf3078cc31e76fc25353901
danielfragareis <danielfreis91@gmail.com>
2023-11-06 08:11:12 -0300

    Alterando a construção da tabela no excel

--- php/src/WebScrapping/Entity/ExportToExcel.php
+++ php/src/WebScrapping/Entity/ExportToExcel.php
@@ -4,23 +4,46 @@ require 'vendor/autoload.php';
 
 use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 
+/**
+ * Class ExportToExcel
+ *
+ * Esta classe é responsável por exportar dados para um arquivo Excel (.xlsx) usando a biblioteca Spout.
+ * Ela permite exportar uma lista de papers com informações detalhadas, incluindo detalhes dos autores, para um arquivo Excel.
+ */
+
 class ExportToExcel
 {
+    /**
+     * Exporta dados para um arquivo Excel.
+     *
+     * @param array $papers   Um array de objetos Paper contendo as informações a serem exportadas.
+     * @param string $filePath O caminho completo do arquivo Excel de destino.
+     *
+     * @throws \Box\Spout\Common\Exception\IOException Caso haja um erro ao criar ou escrever no arquivo Excel.
+     */
     public static function exportToExcel(array $papers, $filePath)
     {
         $writer = WriterEntityFactory::createXLSXWriter();
         $writer->openToFile($filePath);
 
-        $headerRow = WriterEntityFactory::createRowFromArray(['ID', 'Title', 'Type', 'Author 1', 'Author 1 Institution', 'Author 2', 'Author 2 Institution', 'Author 3', 'Author 3 Institution', 'Author 4', 'Author 4 Institution', 'Author 5', 'Author 5 Institution', 'Author 6', 'Author 6 Institution', 'Author 7', 'Author 7 Institution']);
-        $writer->addRow($headerRow);
+        $headerRow = ['ID', 'Title', 'Type'];
 
+        foreach (range(1, 18) as $i) {
+            $headerRow[] = "Author $i";
+            $headerRow[] = "Author ${i} Institution";
+        }
+
+        $writer->addRow(WriterEntityFactory::createRowFromArray($headerRow));
         foreach ($papers as $paper) {
-            $writer->addRow(WriterEntityFactory::createRowFromArray([$paper->id, $paper->title, $paper->type]));
-
+            $rowData = [$paper->id, $paper->title, $paper->type];
+        
             $authors = $paper->authors;
             foreach ($authors as $author) {
-                $writer->addRow(WriterEntityFactory::createRowFromArray(['', '', '', $author->name, $author->institution]));
+                $rowData[] = $author->name;
+                $rowData[] = $author->institution;
             }
+        
+            $writer->addRow(WriterEntityFactory::createRowFromArray($rowData));
         }
 
         $writer->close();
@@ -28,8 +51,4 @@ class ExportToExcel
         echo 'Planilha Excel exportada com sucesso!';
     }
 }
-
-// Exemplo de uso:
-// $papers = [...]; // Preencha com os dados dos papers
-// ExcelExporter::exportToExcel($papers, 'exemplo.xlsx');
 ?>
